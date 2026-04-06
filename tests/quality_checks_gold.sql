68533e0314153177e61a1a842546bed8a5c875f6
brianman515 <114394805+brianman515@users.noreply.github.com>
2026-04-06 13:34:47 +0200

    Refine comments in quality_checks_gold.sql
    Updated comments to improve clarity and conciseness in the quality checks script.

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,17 +1,9 @@
 /*
-===============================================================================
-Quality Checks
-===============================================================================
 Script Purpose:
-    This script performs quality checks to validate the integrity, consistency, 
-    and accuracy of the Gold Layer. These checks ensure:
+    These checks ensure:
     - Uniqueness of surrogate keys in dimension tables.
-    - Referential integrity between fact and dimension tables.
-    - Validation of relationships in the data model for analytical purposes.
-
-Usage Notes:
-    - Investigate and resolve any discrepancies found during the checks.
-===============================================================================
+    - Integrity between fact and dimension tables.
+    - Validation of relationships in the data model
 */
 
 -- ====================================================================
